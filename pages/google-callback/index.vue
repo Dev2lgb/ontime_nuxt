edafb9558f0aa3a578b86e0c70a7a89766e4f2c3
obpark <oesoft52@gmail.com>
2022-11-07 19:37:32 +0900

    google calendar

---
+++ pages/google-callback/index.vue
@@ -0,0 +1,34 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "index",
+  mounted() {
+    this.addGoogleCalendar();
+  },
+  methods: {
+    addGoogleCalendar() {
+      //const query = new URLSearchParams(this.$route.query).toString();
+      const query = Object.entries(this.$route.query).map(e => e.join('=')).join('&');
+      this.$axios.$get('/my/bookings/google-calendar?'+query)
+        .then(data => {
+          if (data.result) {
+            //console.log(data.data);
+            if (data.data.events) {
+              alert('success');
+            }
+            if (data.data.redirect) {
+              this.$router.push(data.data.redirect);
+            }
+          }
+        });
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/test.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-btn @click="addGoogleCalendar">Google Calendar</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "test.vue",
+  methods: {
+    addGoogleCalendar() {
+      this.$axios.$post('/my/bookings/google-calendar', {bookedId: 72, redirect: this.$route.path})
+        .then(data => {
+          if (data.result) {
+            if (data.data.authUrl) {
+              window.location.href = data.data.authUrl
+            }
+            if (data.data.events) {
+              alert('success');
+            }
+          }
+        });
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
